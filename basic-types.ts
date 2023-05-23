e6c901ad0f28d0dbfff1abb199216a31c853b31f
Jeremy Goodman <jeremygoodman52@gmail.com>
2023-05-23 00:19:10 -0500

    nullish operation

--- basic-types.ts
+++ basic-types.ts
@@ -3,6 +3,13 @@ let numbers: number[];
 numbers = [1,2,3,4,5];
 
 
+// !type alias. This is like an object in C#. Adding a ? to the end of a property to make it an optional property.
+type Game = {
+    title: string;
+    genres: string[];
+    price?: number;
+}
+
 // ! arrays have a type of object
 // console.log(typeof numbers)
 
@@ -47,3 +54,31 @@ let personData = {
 let personAge: number | null;
 
 personAge = personData.age;
+
+
+
+let anything = 0;
+
+
+let game: Game = {
+    title: "Super Mario Sunshine",
+    genres: ["platformer", "adventure"],
+    price: 60
+}
+
+console.log(game.price)
+
+
+let gameTwo: Game = {
+    title: "Super Mario Sunshine",
+    genres: ["platformer", "adventure"]
+}
+
+let games: Game[] =[game, gameTwo]
+
+
+// ! having a null value will break the code, unless you use the nullish coalesing operator. this works like a ternary statement in Js. 
+for (const game of games) {
+    console.log((game.price ?? 0) * 100)
+    console.log(game.price?.toString())
+}
