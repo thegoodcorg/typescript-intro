f0d0f0298e24cd4e76a63cb5684d006676977e9a
Jeremy Goodman <jeremygoodman52@gmail.com>
2023-05-27 22:03:30 -0500

    types

--- utility-types.ts
+++ utility-types.ts
@@ -12,7 +12,7 @@ type Book = {
 // ! the partial type has been added to allow using the book type without all the properties.
 const book1: Partial<Book>= {
     title: "Atleus Shrugged",
-    author: "Ayan Rand"
+    author: "Ayan Rand",
 }
 
 // ! The required utility type makes sure the object has all the properties of the base type.
@@ -21,5 +21,22 @@ const book2: Required<Book> = {
     author: "George Orwell",
     genres: ["sci-fi", "dystopian future"],
     eBook: false,
-    readingAge: 18
+    readingAge: 18,
+}
+
+
+// ! Readonly makes it so that the variable or object built cannot be changed later.
+const book3: Readonly<Book> = {
+    title: "Harry Potter",
+    author: "J K Rowling",
+    genres: ["fantasy fiction", "high fantasy"],
+    eBook: true,
+    readingAge: 11,
+}
+
+
+
+const books = {
+    read: [book2],
+    notRead: [book3],
 }
